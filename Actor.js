60ac4ccc0c4399686caa6cca00dc7dcb43b17262
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-15 15:44:45 +0000

    added getters for X and Y

--- Actor.js
+++ Actor.js
@@ -6,7 +6,15 @@ class Actor {
     this.#X = 0;
     this.#Y = 0;
   }
+
+  getX() {
+    return this.#X;
+  }
+
+  getY() {
+    return this.#Y;
+  }
 }
 
 let MyActor = new Actor;
-console.log(MyActor)
+console.log(MyActor.getX())
