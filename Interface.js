d51a88172022ee684265a631a1338dd4298ed41f
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-15 16:56:48 +0000

    showing the x and y values of the actor on the html page

--- Interface.js
+++ Interface.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const updateValues = () => {
+    document.querySelector('#current-values').innerText = actor.getXandY();
+  };
+
+  const actor = new Actor();
+  updateValues();
 
 });
